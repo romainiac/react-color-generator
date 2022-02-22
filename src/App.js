b3b5c780f3704b2245af0374006e1940e6feb5aa
unknown <romanyefimets@gmail.com>
2022-02-22 11:55:08 -0800

    colors are fetched and generated, error is shown if color is invalid

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import Values from 'values.js';
+import SingleColor from './single-color.component';
 
-function App() {
+const App = () => {
+  const [color, setColor] = useState('');
+  const [error, setError] = useState(false);
+  const [list, setList] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    try {
+      let colors = new Values(color).all(10);
+      setList(colors);
+    } catch (error) {
+      setError(true);
+      console.log(error);
+    }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <section className="container">
+        <h3>color generator</h3>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            value={color}
+            onChange={(e) => setColor(e.target.value)}
+            placeholder="#f15025"
+            className={`${error ? 'error' : null}`}
+          />
+          <button className="btn" type="submit">
+            submit
+          </button>
+        </form>
+      </section>
+      <section className="colors">
+        {list.map((color, key) => {
+          return <SingleColor key={key} {...color} index={key} />;
+        })}
+      </section>
+    </>
   );
-}
-
+};
 export default App;
---
+++ src/single-color.component.jsx
@@ -0,0 +1,5 @@
+const SingleColor = ({ rgb, weight, key }) => {
+  return <div>single color</div>;
+};
+
+export default SingleColor;
