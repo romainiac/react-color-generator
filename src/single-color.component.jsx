240d409ed5a3424f48dd91de05c5f40f99359539
unknown <romanyefimets@gmail.com>
2022-02-22 12:22:42 -0800

    finished , app now starts with default value and allows user to copy values to clipboard

--- src/single-color.component.jsx
+++ src/single-color.component.jsx
@@ -1,13 +1,29 @@
+import { useEffect } from 'react';
 import { useState } from 'react';
 
-const SingleColor = ({ rgb, weight, key, hexColor }) => {
+const SingleColor = ({ rgb, weight, index, hexColor }) => {
   const [alert, setAlert] = useState(false);
   const bcg = rgb.join(',');
+  hexColor = '#' + hexColor;
 
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setAlert(false);
+    }, 3000);
+    return () => clearTimeout(timeout);
+  }, [alert]);
   return (
-    <article className={`color`} style={{ backgroundColor: `rgb(${bcg})` }}>
+    <article
+      onClick={() => {
+        setAlert(true);
+        navigator.clipboard.writeText(hexColor);
+      }}
+      className={`color ${index > 10 && 'color-light'}`}
+      style={{ backgroundColor: `rgb(${bcg})` }}
+    >
       <p className="percent-value">{weight}%</p>
-      <p className="color-value">#{hexColor}</p>
+      <p className="color-value">{hexColor}</p>
+      {alert && <p className="alert">copied to clipboard</p>}
     </article>
   );
 };
